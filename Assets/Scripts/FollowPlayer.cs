e0239b2b2c18355101f225f4efae4bad1516abf3
Isaac R.F <isaacrf239@gmail.com>
2021-06-03 21:52:07 +0200

    #1.3 Part 2 - Make camera follow player
    - Create FollowPlayer script with a public GameObject variable called Player
    - Assign Player variable to Vehicle scene object
    - Add an small margin to camera position

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float Speed = 5.0f;
+    public GameObject Player;
     
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+        transform.position = Player.transform.position + new Vector3(-0.2f,5,-8.11f);
     }
 }
